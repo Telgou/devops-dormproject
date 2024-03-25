b0016d3e496901f3bbd8f26bfd6f859848ed04f7
Telgou <gamgamiahmed@yahoo.fr>
2024-03-25 05:53:36 +0100

    .

---
+++ back/src/main/java/tn/esprit/springproject/config/RestServiceCorsApplication.java
@@ -0,0 +1,28 @@
+/*public WebMvcConfigurer corsConfigurer() {
+		@Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://192.168.126.131:4200"));
+        config.setAllowedHeaders(Arrays.asList(
+                ORIGIN,
+                CONTENT_TYPE,
+                ACCEPT,
+                AUTHORIZATION
+        ));
+        config.setAllowedMethods(Arrays.asList(
+                GET.name(),
+                POST.name(),
+                DELETE.name(),
+                PUT.name(),
+                PATCH.name()
+        ));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+
+    }
+
+	}*/
